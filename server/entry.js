2570070a23fc00b910cd7c22088ae56ad27929a6
Calamitizer <cindersquall@gmail.com>
2017-07-06 14:55:33 -0400

    Heroku troubleshooting

--- server/entry.js
+++ server/entry.js
@@ -6,24 +6,25 @@
     var express = require('express');
     var morgan = require('morgan');
     var bodyParser = require('body-parser');
-    var compression = require('compression');
 
     var path = require('path');
     var http = require('http');
 
+    var assetRouter = require('./routers/asset.js');
     var siteRouter = require('./routers/site.js');
 
     var andHow = express();
 
     andHow
-        .use(compression())
         .use(morgan('dev'))
+        .use(bodyParser.json())
         .use(bodyParser.urlencoded({
             extended: true,
         }))
+        .use('/', assetRouter)
         .use('/', siteRouter);
 
-    var port = process.env.port || 3863 // "&?"!
+    var port = process.env.PORT || 3863; // "&?"!
 
     andHow.set('port', port);
     var server = http.createServer(andHow);
---
+++ server/routers/asset.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    var serveStatic = require('serve-static');
+    var path = require('path');
+
+    var root = path.join(__dirname, '../client/www');
+    var options = {
+        index: false,
+    };
+
+    var assetRouter = serveStatic(root, options);
+
+    module.exports = assetRouter
+
+}());
